e731a1f62df2982441ba135dc957be9f591845ba
Stephane Tostivin <stephane.tostivin@gmail.com>
2021-08-17 23:53:47 +0200

    Add PageFactory and fix locators

--- src/test/java/org/example/pages/MakePaymentPage.java
+++ src/test/java/org/example/pages/MakePaymentPage.java
@@ -4,9 +4,11 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,6 +23,7 @@ public class MakePaymentPage {
 
     public MakePaymentPage(AppiumDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
 
--- src/test/java/org/example/pages/PaymentHomePage.java
+++ src/test/java/org/example/pages/PaymentHomePage.java
@@ -3,10 +3,11 @@ package org.example.pages;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,19 +22,20 @@ public class PaymentHomePage {
 
     public PaymentHomePage(AppiumDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
 
     //===============================================================================================
     // Locators
     //===============================================================================================
-    /* The locators can be defined both for the Android and the iOS application
+    /* The mobile locators can be defined both for the Android and the iOS application
        using the appropriate annotation */
     @AndroidFindBy(id = "paymentHomeView")
     private MobileElement paymentHomeScreen;
 
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[1]")
-    private MobileElement balanceText;
+    // This one is a locator for a webelement in a webview - not a mobile element
+    private By balanceText = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[1]");
 
     @AndroidFindBy(id = "makePaymentButton")
     private MobileElement buttonMakePayment;
@@ -58,21 +60,29 @@ public class PaymentHomePage {
         }
     }
 
+    /**
+     * Get the balance displayed in the PaymentHome screen
+     * @return the message with balance amount
+     */
     public String getBalance() {
         String balanceMessage = null;
 
         // The message is displayed after some time, so pls wait...
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        ExpectedCondition<WebElement> balanceMessageReady = ExpectedConditions.visibilityOf(balanceText);
+        ExpectedCondition<WebElement> balanceMessageReady = ExpectedConditions.presenceOfElementLocated(balanceText);
         try {
             wait.until(balanceMessageReady);
-            balanceMessage = balanceText.getText();
+            balanceMessage = driver.findElement(balanceText).getText();
         } catch (SessionNotCreatedException e) {
             logger.log(Level.SEVERE, e.getMessage());
         }
         return balanceMessage;
     }
 
+    /**
+     * Click on the button Make Payment
+     * @return
+     */
     public MakePaymentPage clickMakePaymentButton() {
         buttonMakePayment.click();
         return new MakePaymentPage(driver);
